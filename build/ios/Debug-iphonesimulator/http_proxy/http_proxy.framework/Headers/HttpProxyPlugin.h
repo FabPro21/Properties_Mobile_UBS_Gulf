0157c76c1dee927113d42b17aa635033920e53b3
UmerAbbasi1515 <umerabbasi1515@gmail.com>
2024-05-21 15:26:18 +0500

    updated

--- build/ios/Debug-iphonesimulator/http_proxy/http_proxy.framework/Headers/HttpProxyPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface HttpProxyPlugin : NSObject<FlutterPlugin>
-@end
--- build/ios/Debug-iphonesimulator/ssl_pinning_plugin/ssl_pinning_plugin.framework/Headers/SslPinningPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface SslPinningPlugin : NSObject<FlutterPlugin>
-@end
