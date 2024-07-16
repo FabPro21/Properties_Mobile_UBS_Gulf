71d84ba94f6c4dfd61a5c7660f8aa5d0091e9f9f
UmerAbbasi1515 <umerabbasi1515@gmail.com>
2024-07-16 13:11:13 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:fap_properties/utils/push_notifications_service.dart';
 import 'package:fap_properties/views/auth/splash_screen/splash_screen.dart';
-// import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,7 +11,7 @@ import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:get/get.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:sizer/sizer.dart';
-// import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart';
 
 /* -------------------------------------------------------------------------- */
 /*                     // bypass this Mir Iftikhar says                       */
@@ -38,23 +38,21 @@ Future<void> main() async {
   await Firebase.initializeApp();
 
   // For Production  Must uncomment before go live
-  // await FirebaseAppCheck.instance.activate(
-  //   androidProvider:
-  //       kDebugMode ? AndroidProvider.debug : AndroidProvider.playIntegrity,
-  //   appleProvider: kDebugMode
-  //       ? AppleProvider.debug
-  //       : AppleProvider.appAttestWithDeviceCheckFallback,
-  // );
-  // await FirebaseAppCheck.instance.setTokenAutoRefreshEnabled(true);
-  // await FirebaseAppCheck.instance.getToken();
+  await FirebaseAppCheck.instance.activate(
+    androidProvider:
+        kDebugMode ? AndroidProvider.debug : AndroidProvider.playIntegrity,
+    appleProvider: kDebugMode
+        ? AppleProvider.debug
+        : AppleProvider.appAttestWithDeviceCheckFallback,
+  );
+  await FirebaseAppCheck.instance.setTokenAutoRefreshEnabled(true);
+  await FirebaseAppCheck.instance.getToken();
   // Must uncomment before go live
 
   // for download file
   await FlutterDownloader.initialize(
     debug:
         true, // optional: set to false to disable printing logs to console (default: true)
-    // ignoreSsl:
-    //     true // option: set to false to disable working with http links (default: false)
   );
 
   await dotenv.load(fileName: ".env");
@@ -105,17 +103,12 @@ class _MyAppState extends State<MyApp> {
   //       enableSecure: true,
   //       autoLockAfterSeconds: 0,
   //     ),
-
   //     backgroundColor: Colors.white.withOpacity(0),
   //     blurEffect: PrivacyBlurEffect.light,
   //   );
   //   print(result);
   // }
 
-  /* -------------------------------------------------------------------------- */
-  /*                     // removing this Mir Iftikhar says                     */
-  /* -------------------------------------------------------------------------- */
-// for Feed Back 123
   // Future myCustomImplementation() async {
   //   try {
   //     String checkMsg;
@@ -125,7 +118,7 @@ class _MyAppState extends State<MyApp> {
   //     ];
   //     Map<String, String> headerHttp;
   //     final secure = await HttpCertificatePinning.check(
-  //       serverURL: "https://uatlandlord.fabproperties.ae/",
+  //       serverURL: 'https://landlord.fabproperties.ae',
   //       headerHttp: headerHttp,
   //       sha: SHA.SHA1,
   //       allowedSHAFingerprints: allowedShAFingerprintList,
@@ -146,8 +139,6 @@ class _MyAppState extends State<MyApp> {
   //     return false;
   //   }
   // }
-// 112233
-// sending dumny req no for testing
 
   @override
   Widget build(BuildContext context) {
@@ -156,15 +147,14 @@ class _MyAppState extends State<MyApp> {
     return Sizer(
       builder: (context, orientation, deviceType) {
         return GetMaterialApp(
-          // add builder because want to make indepent app for font size of device
-          localizationsDelegates: [
+         localizationsDelegates: [
             GlobalMaterialLocalizations.delegate,
             GlobalWidgetsLocalizations.delegate,
             GlobalCupertinoLocalizations.delegate,
           ],
           supportedLocales: [
             Locale('en', ''),
-            Locale('ar', ''), // arabic, no country code
+            Locale('ar', ''),
           ],
           builder: (BuildContext context, Widget child) {
             final MediaQueryData data = MediaQuery.of(context).copyWith(
@@ -177,7 +167,6 @@ class _MyAppState extends State<MyApp> {
               child: child,
             );
           },
-          // above lines for restrict the use of device dont
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
               appBarTheme: AppBarTheme(
@@ -193,26 +182,241 @@ class _MyAppState extends State<MyApp> {
             PushNotificationService().setupInteractedMessage();
           },
         );
-        // return GetMaterialApp(
-        //   debugShowCheckedModeBanner: false,
-        //   theme: ThemeData(
-        //       appBarTheme: AppBarTheme(
-        //           backgroundColor: Colors.white,
-        //           systemOverlayStyle: SystemUiOverlayStyle.light),
-        //       backgroundColor: Colors.white,
-        //       scaffoldBackgroundColor: Colors.grey,
-        //       canvasColor: Colors.transparent,
-        //       snackBarTheme:
-        //           SnackBarThemeData(backgroundColor: Colors.white54)),
-        //   home: SplashScreen(),
-        //   onInit: () {
-        //     PushNotificationService().setupInteractedMessage();
-        //   },
-        // );
       },
     );
   }
 }
+
+
+// import 'dart:io';
+// import 'package:fap_properties/utils/push_notifications_service.dart';
+// import 'package:fap_properties/views/auth/splash_screen/splash_screen.dart';
+// // import 'package:firebase_app_check/firebase_app_check.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_downloader/flutter_downloader.dart';
+// import 'package:flutter_phoenix/flutter_phoenix.dart';
+// import 'package:get/get.dart';
+// import 'package:flutter_localizations/flutter_localizations.dart';
+// import 'package:sizer/sizer.dart';
+// // import 'package:flutter/foundation.dart';
+
+// /* -------------------------------------------------------------------------- */
+// /*                     // bypass this Mir Iftikhar says                       */
+// /* -------------------------------------------------------------------------- */
+
+// // Enable Firebase
+// // by using Enable Firebase we can find the place where we can enable/disable firebase
+
+// class MyHttpOverrides extends HttpOverrides {
+//   @override
+//   HttpClient createHttpClient(SecurityContext context) {
+//     return super.createHttpClient(context)
+//       ..badCertificateCallback =
+//           (X509Certificate cert, String host, int port) => true;
+//   }
+// }
+
+// /* -------------------------------------------------------------------------- */
+// /*                     // bypass this Mir Iftikhar says                     */
+// /* -------------------------------------------------------------------------- */
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+
+//   // For Production  Must uncomment before go live
+//   // await FirebaseAppCheck.instance.activate(
+//   //   androidProvider:
+//   //       kDebugMode ? AndroidProvider.debug : AndroidProvider.playIntegrity,
+//   //   appleProvider: kDebugMode
+//   //       ? AppleProvider.debug
+//   //       : AppleProvider.appAttestWithDeviceCheckFallback,
+//   // );
+//   // await FirebaseAppCheck.instance.setTokenAutoRefreshEnabled(true);
+//   // await FirebaseAppCheck.instance.getToken();
+//   // Must uncomment before go live
+
+//   // for download file
+//   await FlutterDownloader.initialize(
+//     debug:
+//         true, // optional: set to false to disable printing logs to console (default: true)
+//     // ignoreSsl:
+//     //     true // option: set to false to disable working with http links (default: false)
+//   );
+
+//   await dotenv.load(fileName: ".env");
+//   HttpOverrides.global = new MyHttpOverrides();
+
+//   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+//       statusBarColor: Colors.transparent,
+//       statusBarIconBrightness: Brightness.light));
+//   SystemChrome.setPreferredOrientations(
+//       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+//   runApp(
+//     Phoenix(
+//       child: MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key key}) : super(key: key);
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   void initState() {
+//     // myCustomImplementation();
+//     // privacyScreen();
+//     super.initState();
+//   }
+
+//   // privacyScreen() when user tap on home button (android) or same in iphone
+//   // app move on background this func will show white screen istead of showing data
+//   // AppIcon
+//   // LaunchImage
+//   // splash
+//   // backgroundImg1
+//   // Future<void> privacyScreen() async {
+//   //   bool result = await PrivacyScreen.instance.enable(
+//   //     iosOptions: const PrivacyIosOptions(
+//   //       enablePrivacy: true,
+//   //       privacyImageName: "",
+//   //       autoLockAfterSeconds: -1,
+//   //       lockTrigger: IosLockTrigger.willResignActive,
+//   //     ),
+//   //     androidOptions: const PrivacyAndroidOptions(
+//   //       enableSecure: true,
+//   //       autoLockAfterSeconds: 0,
+//   //     ),
+
+//   //     backgroundColor: Colors.white.withOpacity(0),
+//   //     blurEffect: PrivacyBlurEffect.light,
+//   //   );
+//   //   print(result);
+//   // }
+
+//   /* -------------------------------------------------------------------------- */
+//   /*                     // removing this Mir Iftikhar says                     */
+//   /* -------------------------------------------------------------------------- */
+// // for Feed Back 123
+//   // Future myCustomImplementation() async {
+//   //   try {
+//   //     String checkMsg;
+//   //     log(AppConfig().baseUrl);
+//   //     List<String> allowedShAFingerprintList = [
+//   //       "9F A6 EF 8C 93 86 50 C9 4D 95 96 ED 2E 84 BF 21 F7 1E C3 AC"
+//   //     ];
+//   //     Map<String, String> headerHttp;
+//   //     final secure = await HttpCertificatePinning.check(
+//   //       serverURL: "https://uatlandlord.fabproperties.ae/",
+//   //       headerHttp: headerHttp,
+//   //       sha: SHA.SHA1,
+//   //       allowedSHAFingerprints: allowedShAFingerprintList,
+//   //       timeout: 50,
+//   //     );
+
+//   //     checkMsg = secure;
+//   //     log("RESULT false: $checkMsg");
+//   //     if (secure.contains("CONNECTION_SECURE")) {
+//   //       log("RESULT true: $checkMsg");
+//   //       return true;
+//   //     } else {
+//   //       log("RESULT false: $checkMsg");
+//   //       return false;
+//   //     }
+//   //   } catch (e) {
+//   //     log("message error == $e");
+//   //     return false;
+//   //   }
+//   // }
+// // 112233
+// // sending dumny req no for testing
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // myCustomImplementation();
+
+//     return Sizer(
+//       builder: (context, orientation, deviceType) {
+//         return GetMaterialApp(
+//           // add builder because want to make indepent app for font size of device
+//           localizationsDelegates: [
+//             GlobalMaterialLocalizations.delegate,
+//             GlobalWidgetsLocalizations.delegate,
+//             GlobalCupertinoLocalizations.delegate,
+//           ],
+//           supportedLocales: [
+//             Locale('en', ''),
+//             Locale('ar', ''), // arabic, no country code
+//           ],
+//           builder: (BuildContext context, Widget child) {
+//             final MediaQueryData data = MediaQuery.of(context).copyWith(
+//               textScaleFactor: 1.0,
+//             );
+//             return MediaQuery(
+//               data: data.copyWith(
+//                 textScaleFactor: 1.0,
+//               ),
+//               child: child,
+//             );
+//           },
+//           // above lines for restrict the use of device dont
+//           debugShowCheckedModeBanner: false,
+//           theme: ThemeData(
+//               appBarTheme: AppBarTheme(
+//                 backgroundColor: Colors.white,
+//               ),
+//               backgroundColor: Colors.white,
+//               scaffoldBackgroundColor: Colors.grey,
+//               canvasColor: Colors.transparent,
+//               snackBarTheme:
+//                   SnackBarThemeData(backgroundColor: Colors.white54)),
+//           home: SplashScreen(),
+//           onInit: () {
+//             PushNotificationService().setupInteractedMessage();
+//           },
+//         );
+//         // return GetMaterialApp(
+//         //   debugShowCheckedModeBanner: false,
+//         //   theme: ThemeData(
+//         //       appBarTheme: AppBarTheme(
+//         //           backgroundColor: Colors.white,
+//         //           systemOverlayStyle: SystemUiOverlayStyle.light),
+//         //       backgroundColor: Colors.white,
+//         //       scaffoldBackgroundColor: Colors.grey,
+//         //       canvasColor: Colors.transparent,
+//         //       snackBarTheme:
+//         //           SnackBarThemeData(backgroundColor: Colors.white54)),
+//         //   home: SplashScreen(),
+//         //   onInit: () {
+//         //     PushNotificationService().setupInteractedMessage();
+//         //   },
+//         // );
+//       },
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//  < ------------------------------------------------------ > //
+
 // import 'dart:io';
 
 // import 'package:fap_properties/utils/push_notifications_service.dart';
