d753a6e62cb8d561d0908466cc2e632d39339d1f
UmerAbbasi1515 <umerabbasi1515@gmail.com>
2025-01-20 11:03:12 +0500

    updated

--- lib/utils/constants/meta_labels.dart
+++ lib/utils/constants/meta_labels.dart
@@ -8,6 +8,7 @@ class AppMetaLabels {
   String login =
       SessionController().getLanguage() == 1 ? "Login" : 'تسجيل الدخول';
   String color = SessionController().getLanguage() == 1 ? "Colour" : 'اللون';
+  String pleaseEnterEmail = SessionController().getLanguage() == 1 ? "Please enter email" : 'اللون';
   String rentPaidAmount = SessionController().getLanguage() == 1
       ? 'Amount of Rent Paid'
       : 'مبلغ الإيجار المدفوع';
