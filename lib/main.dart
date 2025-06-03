95b223895b1e5862ede571995e101a9a4696c7d9
UmerAbbasi1515 <umerabbasi1515@gmail.com>
2025-06-03 20:28:02 +0500

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -77,9 +77,7 @@ Future<void> main() async {
   // for download file
   await FlutterDownloader.initialize(
     debug:
-        true, // optional: set to false to disable printing logs to console (default: true)
-    // ignoreSsl:
-    //     true // option: set to false to disable working with http links (default: false)
+        true, 
   );
 
   await dotenv.load(fileName: ".env");
