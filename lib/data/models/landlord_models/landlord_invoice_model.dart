d8b441351ea9cec9689cf78eb44b3f3d5181b472
umerabbasi1515 <umerabbasi1515@gmail.com>
2025-10-20 13:28:48 +0500

    Complete the flow of android app of development

--- lib/data/models/landlord_models/landlord_invoice_model.dart
+++ lib/data/models/landlord_models/landlord_invoice_model.dart
@@ -96,12 +96,12 @@ class Invoice {
     };
 }
 
-enum InvoiceDate { THE_22032016, THE_31012016, THE_29022016 }
+enum InvoiceDate { the_22032016, the_31012016, the_29022016 }
 
 final invoiceDateValues = EnumValues({
-    "22-03-2016": InvoiceDate.THE_22032016,
-    "29-02-2016": InvoiceDate.THE_29022016,
-    "31-01-2016": InvoiceDate.THE_31012016
+    "22-03-2016": InvoiceDate.the_22032016,
+    "29-02-2016": InvoiceDate.the_29022016,
+    "31-01-2016": InvoiceDate.the_31012016
 });
 
 enum StatusName { UNDER_REVIEW, UNDER_APPROVAL, DRAFT }
