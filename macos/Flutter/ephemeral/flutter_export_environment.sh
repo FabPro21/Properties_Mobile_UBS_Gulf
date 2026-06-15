a662c88dd15a39100712f3cea93afdd1af9925ec
apple <umerabbasi1515@gmail.com>
2026-06-15 10:05:13 +0500

    update keyProperties

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/apple/System/DevTools/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/apple/System/MobileAppRelated/properties_mobile_ubs_gulf"
+export "FLUTTER_APPLICATION_PATH=/Users/apple/System/LatestCodeOfMobileApps/Property/properties_mobile_ubs_gulf"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.21"
